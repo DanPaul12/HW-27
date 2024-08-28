4de4dbd99223c921bd47c7fd62841d997c511e29
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-28 08:30:39 -0700

    hw27

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class Session(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/customer<int:id', methods=['GET'])
+@app.route('/customer<int:id>', methods=['GET'])
 def get_customer(id):
     customer = Customer.query.get_or_404(id)
     return customer_schema.jsonify(customer)
@@ -55,6 +55,33 @@ def add_customer():
         db.session.add(new_customer)
         db.session.commit()
         return jsonify({'message':'customer added'}), 201
+    except:
+        pass
+    finally:
+        pass
+
+@app.route('/customer<int:id>', methods=['PUT'])
+def update_customer(id):
+    customer_data = Customer.query.get_or_404(id)
+    try:
+        customer = customer_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    customer.name = customer_data['name']
+    customer.email = customer_data['email']
+    customer.age = customer_data['age']
+    db.session.commit()
+    return jsonify({"message":'details updated successfully'}), 200
+
+
+
+@app.route('/customer<int:id>', methods=['DELETE'])
+def delete_customer(id):
+    try:
+        customer = Customer.query.get_or_404(id)
+        db.session.delete(customer)
+        db.session.commit()
+        return jsonify({'message': 'customer deleted'})
     except:
         pass
     finally:
